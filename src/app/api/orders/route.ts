38cbd314f7698764d9689b8ee2b2cca85f1513d8
thedon-hub <jeremiahforson5@gmail.com>
2025-05-02 14:11:44 +0000

    order route

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,8 +1,8 @@
 // app/api/orders/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/lib/auth';
-import prisma from '@/lib/prisma';
+import { authOptions } from 'lib/auth';
+import prisma from 'lib/prisma';
 
 export async function GET(request: NextRequest) {
   try {
