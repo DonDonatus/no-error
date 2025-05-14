49d78032c2df88f81235d55aad0e794cf0a7cb18
thedon-hub <jeremiahforson5@gmail.com>
2025-05-14 12:12:25 +0000

    gignore

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 // lib/auth.ts
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "./prisma";
+import {prisma} from "./prisma";
 import { compare } from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
